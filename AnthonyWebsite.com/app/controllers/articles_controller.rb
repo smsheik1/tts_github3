aca56e5f3db51e2eccc08d19840d0d54705c318d
Shahzeb Sheikh <shahzebsheikh54@gmail.com>
2017-12-22 10:25:34 -0600

    this is our 2nd commit

--- AnthonyWebsite.com/app/controllers/articles_controller.rb
+++ AnthonyWebsite.com/app/controllers/articles_controller.rb
@@ -3,13 +3,8 @@ class ArticlesController < ApplicationController
     @articles = Article.order("created_at DESC")
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
 
-  def new
-    @article = Article.new
-  end
+
 
   def create
     @article = Article.new(article_params)
